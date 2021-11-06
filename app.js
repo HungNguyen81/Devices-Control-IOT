0c2dd4845577dfca0f314738fccbd6dab506fbb8
HungNguyen81 <nguyenngochung.ncth@gmail.com>
2021-11-06 22:26:57 +0700

    ok

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
