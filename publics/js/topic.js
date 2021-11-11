142ea08ce02d16478c70a254d95dc783a3384148
HungNguyen81 <nguyenngochung.ncth@gmail.com>
2021-11-12 01:19:41 +0700

    o

--- publics/js/topic.js
+++ publics/js/topic.js
@@ -127,7 +127,8 @@ socket.on(`${topic}/ctrl`, data => {
     let [time, id, stt] = data;
     let checkbox = document.getElementById(`device-${id}`);
     
-    if(stt) checkbox.checked = !checkbox.checked;
+    // if(stt)
+     checkbox.checked = !checkbox.checked;
     toggleSwitch(checkbox);
     console.log(data);
 })
