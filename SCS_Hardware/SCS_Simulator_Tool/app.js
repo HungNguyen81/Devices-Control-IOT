37b1ded37e067fe66e986eade79519fe74d53faf
HungNguyen81 <nguyenngochung.ncth@gmail.com>
2022-01-14 09:20:08 +0700

    fix simulator tool

--- SCS_Hardware/SCS_Simulator_Tool/app.js
+++ SCS_Hardware/SCS_Simulator_Tool/app.js
@@ -6,7 +6,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-app.listen(3000, () => {console.log("Server started!");})
+const port = 3000
+app.listen(port, () => { console.log(`Server started on port ${port}!`); })
 
 var options = {
     host: '1af8e2f5e0ae40308432e82daf1071e0.s1.eu.hivemq.cloud',
@@ -42,12 +43,12 @@ var timeout;
 app.get('/', (req, res) => {
 
     devices = [
-        { id: 1, name:"Device 1", status:0},
-        { id: 2, name:"Device 2", status:0},
-        { id: 3, name:"Device 3", status:0},
-        { id: 4, name:"Device 4", status:0}
+        { id: 1, name: "Device 1", status: 0 },
+        { id: 2, name: "Device 2", status: 0 },
+        { id: 3, name: "Device 3", status: 0 },
+        { id: 4, name: "Device 4", status: 0 }
     ]
-    res.render("index",{devices: devices});
+    res.render("index", { devices: devices });
 })
 app.post('/start', (req, res) => {
     let topic = req.body.topic
@@ -57,20 +58,25 @@ app.post('/start', (req, res) => {
     let humiMax = req.body.maxHumi
     // let cycle = req.body.cycle
 
-    temp = Math.round((Math.random() * (tempMax - tempMin) + tempMin)*100)/100;
-    humid = Math.round((Math.random() * (humiMax - humiMin) + humiMin)*100)/100;
+    temp = Math.round((Math.random() * (tempMax - tempMin) + tempMin) * 100) / 100;
+    humid = Math.round((Math.random() * (humiMax - humiMin) + humiMin) * 100) / 100;
 
     client.publish(`scs/${topic}/data`, `temp ${temp}`)
     client.publish(`scs/${topic}/data`, `humid ${humid}`)
-    res.status(200).json({ msg: [`temp ${temp}`,  `humid ${humid}`]})
+    res.status(200).json({ msg: [`temp ${temp}`, `humid ${humid}`] })
 })
 
 
-app.post('/control', (req, res)=>{
-    let topic = req.body.topic
-    let id = req.body.id
-    let stt = req.body.stt
-    console.log(req.body);
-    client.publish(`scs/${topic}`, `ctrl ${id} ${stt}`);
-    res.status(200)
+app.post('/control', (req, res) => {
+    try {
+        let topic = req.body.topic
+        let id = req.body.id
+        let stt = req.body.stt
+        console.log(req.body);
+        client.publish(`scs/${topic}`, `ctrl ${id} ${stt}`);
+        res.status(200).end()
+    } catch (error) {
+        console.log(error);
+        res.status(500).end()
+    }
 })
